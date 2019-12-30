d98595cc0b7327fe7cad0054ccbfd9b86a016d06
jacob <337581468@.com>
2019-12-30 19:27:08 +0800

    扩展SpringMvc,拦截请求

---
+++ src/main/java/com/example/bilibili/config/WebMvcCongfig.java
@@ -0,0 +1,30 @@
+package com.example.bilibili.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+
+//使用WebMvcConfigurerAdapter可以扩展
+@Configuration
+public class WebMvcCongfig extends WebMvcConfigurerAdapter {
+//    @Override
+//    public void addViewControllers(ViewControllerRegistry registry) {
+////        super.addViewControllers(registry);
+//        registry.addViewController("/text").setViewName("success");
+//    }
+
+    @Bean //将组件组册在容器
+    public WebMvcConfigurerAdapter webMvcConfigurerAdapter() {
+        WebMvcConfigurerAdapter adpater = new WebMvcConfigurerAdapter() {
+            @Override
+            public void addViewControllers(ViewControllerRegistry registry) {
+                registry.addViewController("/text").setViewName("success");
+                registry.addViewController("/").setViewName("index");
+            }
+        };
+        return adpater;
+    }
+
+}
